ddb61e3fb1262e42615c20822d062723e6b869ac
Ciarenn Hollis <94020137+CiarennHollis@users.noreply.github.com>
2024-02-19 16:12:02 -0600

    Adds comment to the croc manager script.

--- Assets/Scripts/CrocManager.cs
+++ Assets/Scripts/CrocManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class CrocManager : MonoBehaviour
 {
+    /// <summary>
+    /// Manages the crocs
+    ///     - triggers them to move up
+    ///     - selects the crocs 
+    ///     - holds the list of them
+    /// </summary>
     // P R O P E R T I E S
     [SerializeField] public GameObject[] Crocs;
 
