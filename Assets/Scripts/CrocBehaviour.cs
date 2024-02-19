8590264de40abdb763768603f731bfb6b5e1a0f2
Krano Network <54424202+KranoNetwork@users.noreply.github.com>
2024-02-19 14:31:33 -0600

    Rework Begun

---
+++ Assets/Scripts/CrocBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrocBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+
+    [SerializeField] public GameObject[] Crocs;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
