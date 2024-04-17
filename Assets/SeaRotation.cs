d8b9360e5f73dee5b35e3eb00c571a50f7a5d591
Krano Network <Xluzue@gmail.com>
2024-04-17 17:44:17 -0500

    Second Push for Enviorment

---
+++ Assets/SeaRotation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaRotation : MonoBehaviour
+{
+    [Range(0f, 2f)]
+    [SerializeField]
+    float maxrotation = 2;
+
+    [Range(1f, 200f)]
+    [SerializeField]
+    float RotationSpeed = 100;
+
+    [Range(0, 2)]
+    [SerializeField]
+    int Axis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Axis == 0)
+        {
+            this.gameObject.transform.Rotate(maxrotation * Mathf.Sin(Time.deltaTime * (RotationSpeed / 100)), 0, 0);
+        }
+        else if (Axis == 1)
+        {
+            this.gameObject.transform.Rotate(0, maxrotation * Mathf.Sin(Time.deltaTime * (RotationSpeed / 100)), 0);
+        }
+        else
+        {
+            this.gameObject.transform.Rotate(0, 0, maxrotation * Mathf.Sin(Time.deltaTime * (RotationSpeed / 100)));
+        }
+    }
+}
